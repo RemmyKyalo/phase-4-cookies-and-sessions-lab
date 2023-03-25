f4b811b14b65b38c3b09e8ddde9b915d094fc2fa
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-25 16:30:25 +0300

    commit tree 45297cf23a0f9e694affecb0abad91fcc4104562

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+#   def index
+#     session[:page_views] ||= 0
+#     cookies[:cookies_view] ||= "World"
+#     render json: { session: session, cookies: cookies.to_hash }
+#   end
+end
